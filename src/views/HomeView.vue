8684610f108819a9fda9ec470809718969658c75
thompson <alfredthompsonovie@gmail.com>
2022-11-08 22:52:40 +0100

    homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,172 @@
 <template>
-  <main>
-    Home Page
-  </main>
+	<main class="main">
+		<section class="hero">
+			<section class="hero__illustration">
+				<div class="illustration"></div>
+			</section>
+			<section class="hero__content">
+				<div class="hero__content--inner">
+					<h1 class="heading heading--hero">
+						<span>Humanizing</span> <span class="block">your insurance.</span>
+					</h1>
+					<p class="text text--hero">
+						Get your life insurance coverage easier and faster. We blend our
+						expertise and technology to help you find the plan that’s right for
+						you. Ensure you and your loved ones are protected.
+					</p>
+					<a href="#" class="cta">View plans</a>
+				</div>
+			</section>
+		</section>
+		<section class="about grid">
+			<section class="about--inner">
+				<h1 class="heading heading--about">We’re different</h1>
+				<section class="card__container">
+					<div class="card">
+						<img
+							src="images/icon-snappy-process.svg"
+							alt="Snappy process icon"
+						/>
+						<h2 class="heading">Snappy Process</h2>
+						<p class="text">
+							Our application process can be completed in minutes, not hours.
+							Don’t get stuck filling in tedious forms.
+						</p>
+					</div>
+					<div class="card">
+						<img
+							src="images/icon-affordable-prices.svg"
+							alt="Affordable Prices icon"
+						/>
+						<h2 class="heading">Affordable Prices</h2>
+						<p class="text">
+							We don’t want you worrying about high monthly costs. Our prices
+							may be low, but we still offer the best coverage possible.
+						</p>
+					</div>
+					<div class="card">
+						<img src="images/icon-people-first.svg" alt="People first icon" />
+						<h2 class="heading">People First</h2>
+						<p class="text">
+							Our plans aren’t full of conditions and clauses to prevent
+							payouts. We make sure you’re covered when you need it.
+						</p>
+					</div>
+				</section>
+				<section class="moreInfo">
+					<h1 class="heading">Find out more <span>about how we work</span></h1>
+					<a href="#" class="cta">How we work</a>
+				</section>
+			</section>
+		</section>
+	</main>
+	<footer class="footer">
+		<section class="footer__social">
+			<a href="#" class="homePage">
+				<img src="images/logo.svg" alt="Insure logo" />
+			</a>
+			<ul class="social__list">
+				<li class="social__item">
+					<a href="#" class="social__link">
+						<img
+							src="images/icon-facebook.svg"
+							alt="facebook logo"
+							class="social__link--img"
+						/>
+					</a>
+				</li>
+				<li class="social__item">
+					<a href="#" class="social__link">
+						<img
+							src="images/icon-twitter.svg"
+							alt="twitter logo"
+							class="social__link--img"
+						/>
+					</a>
+				</li>
+				<li class="social__item">
+					<a href="#" class="social__link">
+						<img
+							src="images/icon-pinterest.svg"
+							alt="pinterest logo"
+							class="social__link--img"
+						/>
+					</a>
+				</li>
+				<li class="social__item">
+					<a href="#" class="social__link">
+						<img
+							src="images/icon-instagram.svg"
+							alt="instagram logo"
+							class="social__link--img"
+						/>
+					</a>
+				</li>
+			</ul>
+		</section>
+		<section class="footer__nav__bar">
+			<ul class="footer__nav__list">
+				<li class="footer__nav__item">Our company</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">How we work</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Why Insure?</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">View plans</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Reviews</a>
+				</li>
+			</ul>
+			<ul class="footer__nav__list">
+				<li class="footer__nav__item">Help me</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">FAQ</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link"> Terms of use</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Privacy policy</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Cookies</a>
+				</li>
+			</ul>
+			<ul class="footer__nav__list">
+				<li class="footer__nav__item">Contact</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Sales</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Support</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Live chat</a>
+				</li>
+			</ul>
+			<ul class="footer__nav__list">
+				<li class="footer__nav__item">Others</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Careers</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Press</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Licenses</a>
+				</li>
+			</ul>
+		</section>
+		<div class="attribution">
+			Challenge by
+			<a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+				>Frontend Mentor</a
+			>. Coded by <a href="#">Your Name Here</a>.
+		</div>
+	</footer>
 </template>
 
-<script>
-
-</script>
+<script></script>
