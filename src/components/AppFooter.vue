52c025f29902e86081d9ff58232ea2a0fedba7d2
thompson <alfredthompsonovie@gmail.com>
2022-11-09 00:01:19 +0100

    footer components

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,15 +1,117 @@
 <template>
-  <div>
-
-  </div>
+	<footer class="footer">
+		<section class="footer__social">
+			<a href="#" class="homePage">
+				<img src="@/assets/images/logo.svg" alt="Insure logo" />
+			</a>
+			<ul class="social__list">
+				<li class="social__item">
+					<a href="#" class="social__link">
+						<img
+							src="@/assets/images/icon-facebook.svg"
+							alt="facebook logo"
+							class="social__link--img"
+						/>
+					</a>
+				</li>
+				<li class="social__item">
+					<a href="#" class="social__link">
+						<img
+							src="@/assets/images/icon-twitter.svg"
+							alt="twitter logo"
+							class="social__link--img"
+						/>
+					</a>
+				</li>
+				<li class="social__item">
+					<a href="#" class="social__link">
+						<img
+							src="@/assets/images/icon-pinterest.svg"
+							alt="pinterest logo"
+							class="social__link--img"
+						/>
+					</a>
+				</li>
+				<li class="social__item">
+					<a href="#" class="social__link">
+						<img
+							src="@/assets/images/icon-instagram.svg"
+							alt="instagram logo"
+							class="social__link--img"
+						/>
+					</a>
+				</li>
+			</ul>
+		</section>
+		<section class="footer__nav__bar">
+			<ul class="footer__nav__list">
+				<li class="footer__nav__item">Our company</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">How we work</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Why Insure?</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">View plans</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Reviews</a>
+				</li>
+			</ul>
+			<ul class="footer__nav__list">
+				<li class="footer__nav__item">Help me</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">FAQ</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link"> Terms of use</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Privacy policy</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Cookies</a>
+				</li>
+			</ul>
+			<ul class="footer__nav__list">
+				<li class="footer__nav__item">Contact</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Sales</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Support</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Live chat</a>
+				</li>
+			</ul>
+			<ul class="footer__nav__list">
+				<li class="footer__nav__item">Others</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Careers</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Press</a>
+				</li>
+				<li class="footer__nav__item">
+					<a href="#" class="footer__nav__link">Licenses</a>
+				</li>
+			</ul>
+		</section>
+		<div class="attribution">
+			Challenge by
+			<a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+				>Frontend Mentor</a
+			>. Coded by <a href="#">Your Name Here</a>.
+		</div>
+	</footer>
 </template>
 
 <script>
-  export default {
-    name: "AppFooter"
-  }
+export default {
+	name: "AppFooter",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
